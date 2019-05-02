f591ccbf5782c0b1b7a709c2ec975c84a09d32ac
Your Name <droni2442@gmail.com>
2019-05-02 16:39:06 +0300

    s

---
+++ precache-manifest.3db97b2ef5eeebc9a316203029d212cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc23b881aa870bbbcd7295e0da96fdb",
+    "url": "/static/media/img_test.6cc23b88.png"
+  },
+  {
+    "revision": "1f3258e94e359bc6908d6496e553b7e3",
+    "url": "/static/media/img_intro.1f3258e9.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c07297acfbb42092d35",
+    "url": "/static/js/main.ff557970.chunk.js"
+  },
+  {
+    "revision": "047546e76d125ebcc4c7",
+    "url": "/static/js/2.70c51355.chunk.js"
+  },
+  {
+    "revision": "8c07297acfbb42092d35",
+    "url": "/static/css/main.f1fe555d.chunk.css"
+  },
+  {
+    "revision": "96761d37cc20a42153deae5d8088c4fe",
+    "url": "/index.html"
+  }
+];
